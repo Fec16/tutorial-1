41fc63eae5a09ff897aac21189a11b783ee23edf
Fec16 <jasonkentwinata@gmail.com>
2024-02-07 17:25:36 +0700

    Implement EditProduct

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -35,4 +35,17 @@ public class ProductController {
         model.addAttribute("products", allProducts);
         return "ListProduct";
     }
+
+    @GetMapping("/edit/{id}")
+    public String editProductPage(@PathVariable String id, Model model) {
+        Product product = service.findById(id);
+        model.addAttribute("product", product);
+        return "EditProduct";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String editProductPost(@ModelAttribute Product product, Model model, @PathVariable String id) {
+        service.update(id, product);
+        return "redirect:/product/list";
+    }
 }
