13ff08cf428e437ab4e8cff86b730ef95993acba
Fec16 <jasonkentwinata@gmail.com>
2024-02-14 17:48:43 +0700

    Fix sonarcloud

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,10 +21,8 @@ version = "0.0.1-SNAPSHOT"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_21
-    sourceCompatibility = JavaVersion.VERSION_17
     toolchain {
         languageVersion = JavaLanguageVersion.of(21)
-        languageVersion = JavaLanguageVersion.of(17)
     }
 }
 
