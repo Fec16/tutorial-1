23e0081eceff4f726848f5b5ddaede8daf7feef5
Fec16 <jasonkentwinata@gmail.com>
2024-03-03 02:26:58 +0700

    [REFACTOR] Apply OrderStatus enum to Order model tests

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import enums.OrderStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -38,7 +39,7 @@ public class OrderTest {
     @Test
     void testCreateOrderDefaultStatus() {
         Order order = new Order ("13652556-012a-4c07-b546-54eb1396d79b",
-                this.products, 1788568000L, "Safira Sudrajat");
+                this.products, 1708560000L, "Safira Sudrajat");
 
         assertSame(this. products, order.getProducts());
         assertEquals(2, order.getProducts().size());
@@ -46,7 +47,7 @@ public class OrderTest {
         assertEquals("Sabun Cap Usep", order.getProducts().get(1).getProductName()) ;
 
         assertEquals("13652556-012a-4c07-b546-54eb1396d79b", order.getId());
-        assertEquals(1788568000L, order.getOrderTime());
+        assertEquals(1708560000L, order.getOrderTime());
         assertEquals("Safira Sudrajat", order.getAuthor());
         assertEquals("WAITING_PAYMENT", order.getStatus());
     }
@@ -54,9 +55,9 @@ public class OrderTest {
     @Test
     void testCreateOrderSuccessStatus() {
         Order order = new Order ("13652556-012a-4c07-b546-54eb1396d79b",
-                this.products, 1788568000L, "Safira Sudrajat", "SUCCESS");
-
-        assertEquals("SUCCESS", order.getStatus());
+                this.products, 1788568000L, "Safira Sudrajat",
+                OrderStatus.SUCCESS.getValue());
+        assertEquals(OrderStatus.SUCCESS.getValue(), order.getStatus());
     }
 
     @Test
