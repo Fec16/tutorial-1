f772f923f3fa50d797b7211bab796728e0d8a6b6
Fec16 <jasonkentwinata@gmail.com>
2024-03-06 18:58:37 +0700

    [REFACTOR] Add PaymentCashOnDelivery

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentCashOnDelivery.java
@@ -0,0 +1,18 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+
+import java.util.Map;
+
+public class PaymentCashOnDelivery extends Payment{
+    public PaymentCashOnDelivery(String id, Order order, Map<String, String> paymentData) {
+        super(id, order, paymentData);
+    }
+
+    @Override
+    boolean paymentDataIsValid() {
+        String address = paymentData.get("address");
+        String deliveryFee = paymentData.get("deliveryFee");
+
+        return address != null && !address.isEmpty() && deliveryFee != null && !deliveryFee.isEmpty();
+    }
+}
