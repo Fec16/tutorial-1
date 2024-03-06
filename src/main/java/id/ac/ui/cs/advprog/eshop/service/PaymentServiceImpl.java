042593cc204469cb5cd94efcfdb012f0980ff926
Fec16 <jasonkentwinata@gmail.com>
2024-03-06 19:43:37 +0700

    [GREEN] Implement PaymentServiceImpl class

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -0,0 +1,51 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import id.ac.ui.cs.advprog.eshop.model.PaymentCashOnDelivery;
+import id.ac.ui.cs.advprog.eshop.model.PaymentVoucherCode;
+import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+@Service
+public class PaymentServiceImpl implements PaymentService{
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+    public Payment addPayment (String id, Order order, String method, Map<String, String> paymentData){
+        if (paymentRepository.findById(id) == null){
+
+            Payment payment = switch (method) {
+                case "VOUCHER_CODE" -> new PaymentVoucherCode(id, order, paymentData);
+                case "CASH_ON_DELIVERY" -> new PaymentCashOnDelivery(id, order, paymentData);
+                default -> throw new IllegalArgumentException();
+            };
+            paymentRepository.save(payment);
+            return payment;
+        }
+        return null;
+    }
+    public Payment setStatus(Payment payment, String status){
+        Payment savedPayment = paymentRepository.findById(payment.getId());
+        if (savedPayment != null){
+
+            savedPayment.setStatus(status);
+            paymentRepository.save(savedPayment);
+            return savedPayment;
+
+        }else{
+            throw new NoSuchElementException();
+        }
+    }
+    public Payment getPayment(String paymentId){
+        return paymentRepository.findById(paymentId);
+    }
+    public List<Payment> getAllPayments(){
+        return paymentRepository.getAllPayment();
+    }
+}
