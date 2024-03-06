f26f1165b82ba5042baee649cbe3abd490e2e3d3
Fec16 <jasonkentwinata@gmail.com>
2024-03-06 20:19:06 +0700

    [REFACTOR] Fix Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -5,51 +5,43 @@ import lombok.Getter;
 
 import java.util.Map;
 
-
 @Getter
-public abstract class Payment {
+public class Payment {
 
     String id;
     Order order;
     Map<String, String> paymentData;
-
     String status;
-
-
 
     public Payment(String id, Order order, Map<String, String> paymentData) {
         this.id = id;
         this.order = order;
         this.paymentData = paymentData;
 
-
-        if (paymentDataIsValid()){
+        if (paymentDataIsValid()) {
             setStatus("SUCCESS");
-        } else{
+        } else {
             setStatus("REJECTED");
         }
     }
-    public Payment(String id, Order order, Map<String, String> paymentData, String status) {
-        this.id = id;
-        this.order = order;
-        this.paymentData = paymentData;
-        setStatus(status);
-    }
-    public void setStatus(String status){
-        if (PaymentStatus.contains(status)){
+
+    public void setStatus(String status) {
+        if (PaymentStatus.contains(status)) {
             this.status = status;
 
-            if (status.equals("SUCCESS")){
+            if (status.equals("SUCCESS")) {
                 order.setStatus("SUCCESS");
-            } else if (status.equals("REJECTED")){
+            } else if (status.equals("REJECTED")) {
                 order.setStatus("FAILED");
             }
 
-        } else{
+        } else {
             throw new IllegalArgumentException();
         }
-
     }
 
-    abstract boolean paymentDataIsValid();
+    // Default implementation of paymentDataIsValid
+    boolean paymentDataIsValid() {
+        return true; // Default implementation always returns true
+    }
 }
--- src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentVoucherCode.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentVoucherCode.java
@@ -2,7 +2,7 @@ package id.ac.ui.cs.advprog.eshop.model;
 
 import java.util.Map;
 
-public class PaymentVoucherCode extends Payment{
+public class PaymentVoucherCode extends Payment {
     public PaymentVoucherCode(String id, Order order, Map<String, String> paymentData) {
         super(id, order, paymentData);
     }
@@ -15,8 +15,8 @@ public class PaymentVoucherCode extends Payment{
         }
 
         int numCharCount = 0;
-        for (int i = 0; i < voucherCode.length(); i++){
-            if (Character.isDigit(voucherCode.charAt(i))){
+        for (int i = 0; i < voucherCode.length(); i++) {
+            if (Character.isDigit(voucherCode.charAt(i))) {
                 numCharCount++;
             }
         }
